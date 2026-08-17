d5411ad4f3a25084d92a668632f1598a1bf5a333
Vadym Nersesov <nersesov.vadim@gmail.com>
2026-08-17 10:20:45 +0200

    Release v5.2-r2

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tool_selfsignuphardlifecycle';
-$plugin->version = 2026081300;
-$plugin->release = 'v5.2-r1';
+$plugin->version = 2026081301;
+$plugin->release = 'v5.2-r2';
 $plugin->requires = 2026042000;
 $plugin->supported = [502, 502];
 $plugin->maturity = MATURITY_STABLE;
